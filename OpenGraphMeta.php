548c88f08a985e4a90829603170b5b4d1bd96a97
James Haley <haleyjd@hotmail.com>
2019-03-28 01:23:45 -0500

    Work on adding support for JSON+LD and meta keywords tag

--- OpenGraphMeta.php
+++ OpenGraphMeta.php
@@ -28,6 +28,7 @@ $wgExtensionMessagesFiles['OpenGraphMeta'] = $dir . '/OpenGraphMeta.i18n.php';
 $wgHooks['ParserFirstCallInit'][] = 'efOpenGraphMetaParserInit';
 function efOpenGraphMetaParserInit( $parser ) {
 	$parser->setFunctionHook( 'setmainimage', 'efSetMainImagePF' );
+	$parser->setFunctionTagHook( 'metakeys', 'OpenGraphMetaSEO::parseKeywordsTag', Parser::SFH_OBJECT_ARGS );
 	return true;
 }
 
@@ -108,11 +109,217 @@ class OpenGraphMetaPageImage
 		wfGetMainCache()->delete( $cacheKey );
 		return true;
 	}
+	
+	// Set og:image property in the meta array
+	public static function getSocialImage($title, $out, &$meta) {
+		global $wgLogo;
+		$isMainpage = $title->isMainPage();
+		if (isset( $out->mMainImage ) && ( $out->mMainImage !== false ) ) {
+			$meta["og:image"] = self::getThumbUrl( $out->mMainImage );
+		} elseif ( $isMainpage ) {
+			$meta["og:image"] = wfExpandUrl($wgLogo);
+		} elseif ( $title->inNamespace( NS_FILE ) ) { // haleyjd: NS_FILE is trivial
+			$file = wfFindFile( $title->getDBkey() );
+			if ( $file && self::isAllowedThumb( $file ) ) {
+				$meta["og:image"] = self::getThumbUrl( $file );
+			}
+		} elseif ( defined('PAGE_IMAGES_INSTALLED') ) { // haleyjd: integrate with Extension:PageImages
+			self::getPageImage( $meta, $title );
+		}
+	}
 }
+
+// haleyjd: General SEO enhancements; derived from Curse SEO extension
+class OpenGraphMetaSEO
+{
+	public static $itemprop = [];
+	
+	/**
+	 * Adds itemprop attributes to the <head>.
+	 *
+	 * @access	public
+	 * @param	object	Output Object
+	 * @param	object	Skin Object
+	 * @return	boolean	True
+	 */
+	public static function onBeforePageDisplay(OutputPage &$out, &$skin = false) {
+		global $wgSitename;
+		//Author
+		$out->addHeadItem('itemprop-author', '<meta itemprop="author" content="' . $wgSitename . '" />');
+
+		if (isset($out->mItemprops) && is_array($out->mItemprops)) {
+			foreach ($out->mItemprops as $itemprop) {
+				$out->addHeadItem('itemprop-'.md5($itemprop['name'].'-'.$itemprop['content']), '<meta itemprop="'.$itemprop['itemprop'].'" content="'.$itemprop['content'].'" />');
+			}
+		}
+
+		//Keywords
+		if (isset($out->mKeywords)) {
+			$out->addMeta('keywords', implode(',', $out->mKeywords));
+		}
+
+		return true;
+	}
+	
+	/**
+	 * Modify template variables.
+	 *
+	 * @access	public
+	 * @param	object	SkinTemplate Object
+	 * @param	object	Initialized QuickTemplate Object
+	 * @return	boolean True
+	 */
+	public static function onSkinTemplateOutputPageBeforeExec(&$skinTemplate, &$template) {
+		global $wgSitename, $wgLogo, $egSocialSettings;
+
+		if (isset($template->data['headelement'])) {
+			$timestamp = $skinTemplate->getOutput()->getRevisionTimestamp();
+
+			// No cached timestamp, load it from the database
+			if ($timestamp === null) {
+				$timestamp = Revision::getTimestampFromId($skinTemplate->getTitle(), $skinTemplate->getRevisionId());
+			}
+			
+			// Get page image info
+			$meta = array();
+			OpenGraphMetaPageImage::getSocialImage($skinTemplate->getTitle(), $skinTemplate->getOutput(), $meta);
+			
+			// Get site logo
+			$file = wfLocalFile(Title::newFromText('File:Wiki.png'));
+
+			$width = 150;
+			$height = 150;
+			$image = $wgLogo;
+			$cacheHash = '';
+			if ($file !== null && $file->exists()) {
+				$cacheHash = '?version='.md5($file->getTimestamp().$file->getWidth().$file->getHeight());
+				$width = $file->getWidth();
+				$height = $file->getHeight();
+				$image = $file->getFullUrl();
+			}
+
+			$searchPage = Title::newFromText('Special:Search');
+			$search = $searchPage->getFullUrl(['search' => 'search_term'], false, PROTO_HTTPS);
+			$search = str_replace('search_term', '{search_term}', $search);
+
+			$profiles = '"'.implode('", "', $egSocialSettings['profile_urls']).'"';
+
+			$ldJson = '
+<script type="application/ld+json">
+{
+	"@context": "http://schema.org/",
+	"@type": "Article",
+	"name": "'.$template->get('title').'",
+	"headline": "'.$template->get('title').'",
+	"image": {
+		"@type": "ImageObject",
+		"url": "'.$meta['og:image'].'",
+	},
+	"author": {
+		"@type": "Organization",
+		"name": "'.$wgSitename.'"
+	},
+	"publisher": {
+		"@type": "Organization",
+		"name": "'.$wgSitename.'",
+		"logo": {
+			"@type": "ImageObject",
+			"url": "'.$image.$cacheHash.'",
+			"width": "'.$width.'",
+			"height": "'.$height.'"
+		},
+		"sameAs": [
+			'.$profiles.'
+		]
+	},
+	"potentialAction": {
+		"@type": "SearchAction",
+		"target": "'.$search.'",
+		"query-input": "required name=search_term"
+	},
+	"datePublished": "'.wfTimestamp(TS_ISO_8601, $timestamp).'",
+	"dateModified": "'.wfTimestamp(TS_ISO_8601, $timestamp).'",
+	"mainEntityOfPage": "'.$skinTemplate->getTitle()->getFullUrl('', false, PROTO_HTTPS).'"
+}
+</script>
+';
+
+			$template->set('headelement', $template->data['headelement'].$ldJson);
+		}
+	}
+
+	/**
+	 * Adds itemprop attributes to HTML
+	 *
+	 * @access	public
+	 * @param	object	OutputPage
+	 * @param	object  ParserOutput
+	 * @return	void
+	 */
+	public static function addTagsToHead($out, $parserOutput) {
+		if (!isset($out->mItemprops)) {
+			$out->mItemprops = [];
+		}
+
+		$out->mItemprops = $parserOutput->getProperty('itemprops');
+	}
+	
+	/**
+	 * gets keywords and adds them to object
+	 *
+	 * @access	public
+	 * @param	object	Parser Object
+	 * @param	object  frame
+	 * @param   string of content
+	 * @return	return message on error
+	 */
+	public static function parseKeywordsTag(Parser $parser, $frame, $content) {
+		if (empty($content)) {
+			return wfMessage('seo_keywords_missing')->inContentLanguage()->plain();
+		}
+		
+		$out = $parser->getOutput();
+		$out->setProperty('keywords', $content);
+	}
+	
+	/**
+	 * Adds keywords attributes to HTML
+	 *
+	 * @access	public
+	 * @param	object	OutputPage
+	 * @param	object	ParserOutput
+	 * @return	void
+	 */
+	public static function addKeywordsToHead($out, $parserOutput) {
+		if (!isset($out->mKeywords)) {
+			$out->mKeywords = array();
+		}
+		$out->mKeywords = array_map("trim", explode(',', $parserOutput->getProperty('keywords')));
+	}
+	
+	/**
+	 * OutputPage ParserOutput Hook that puts all parseroutput page hooks into action.
+	 *
+	 * @see		https://www.mediawiki.org/wiki/Manual:Hooks/OutputPageParserOutput
+	 * @access	public
+	 * @param	OutputPage		$out
+	 * @param	ParserOutput	$parseroutput
+	 * @return	boolean
+	 */
+	public static function onOutputPageParserOutput(OutputPage &$out, ParserOutput $parseroutput) {
+		self::addKeywordsToHead($out, $parseroutput);
+		self::addTagsToHead($out, $parseroutput);
+		return true;
+	}
+}
+
+// haleyjd: Add additional hooks for SEO
+$wgHooks['OutputPageParserOutput'          ][] = 'OpenGraphMetaSEO::onOutputPageParserOutput';
+$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'OpenGraphMetaSEO::onSkinTemplateOutputPageBeforeExec';
 
 $wgHooks['BeforePageDisplay'][] = 'efOpenGraphMetaPageHook';
 function efOpenGraphMetaPageHook( &$out, &$sk ) {
-	global $wgLogo, $wgSitename, $wgXhtmlNamespaces, $egFacebookAppId, $egFacebookAdmins;
+	global $wgSitename, $wgXhtmlNamespaces, $egFacebookAppId, $egFacebookAdmins;
 	$wgXhtmlNamespaces["og"] = "http://opengraphprotocol.org/schema/";
 	$title = $out->getTitle();
 	$isMainpage = $title->isMainPage();
@@ -134,18 +341,9 @@ function efOpenGraphMetaPageHook( &$out, &$sk ) {
 		}
 	}
 
-	if ( isset( $out->mMainImage ) && ( $out->mMainImage !== false ) ) {
-		$meta["og:image"] = OpenGraphMetaPageImage::getThumbUrl( $out->mMainImage );
-	} elseif ( $isMainpage ) {
-		$meta["og:image"] = wfExpandUrl($wgLogo);
-	} elseif ( $title->inNamespace( NS_FILE ) ) { // haleyjd: NS_FILE is trivial
-		$file = wfFindFile( $title->getDBkey() );
-		if ( $file && OpenGraphMetaPageImage::isAllowedThumb( $file ) ) {
-			$meta["og:image"] = OpenGraphMetaPageImage::getThumbUrl( $file );
-		}
-	} elseif ( defined('PAGE_IMAGES_INSTALLED') ) { // haleyjd: integrate with Extension:PageImages
-		OpenGraphMetaPageImage::getPageImage( $meta, $title );
-	}
+	// haleyjd: Get og:image property
+	OpenGraphMetaPageImage::getSocialImage($title, $out, $meta);
+	
 	if ( isset($out->mDescription) ) { // set by Description2 extension, install it if you want proper og:description support
 		$meta["og:description"] = $out->mDescription;
 	}
@@ -166,10 +364,14 @@ function efOpenGraphMetaPageHook( &$out, &$sk ) {
 			}
 		}
 	}
+	
+	// haleyjd: Add additional SEO items
+	OpenGraphMetaSEO::onBeforePageDisplay($out, $sk);
 
 	return true;
 }
 
 $egFacebookAppId = null;
 $egFacebookAdmins = null;
+$egSocialSettings = array();
 
